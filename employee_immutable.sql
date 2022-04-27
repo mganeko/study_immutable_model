83d725d903d28aca3e6854c95384b709ef1626e1
mganeko <m.ganeko@gmail.com>
2022-04-27 15:32:01 +0900

    another query

--- employee_immutable.sql
+++ employee_immutable.sql
@@ -532,6 +532,20 @@ SELECT asgn.employee_number, emp.name, department_id, dpt.name, position
  FROM assign AS asgn
   INNER JOIN Employee AS emp ON asgn.employee_number = emp.employee_number
   INNER JOIN Department AS dpt ON asgn.department_id = dpt.id;
+
+WITH assign
+AS (
+  SELECT employee_number, department_id, position, SUM(assign_value) AS assign_summary
+   FROM Assign_Levae
+    WHERE move_date <= '2023-05-01'
+   GROUP BY employee_number, department_id, position
+   HAVING assign_summary > 0
+)
+SELECT asgn.employee_number, emp.name, department_id, dpt.name, position
+ FROM assign AS asgn
+  INNER JOIN Employee AS emp ON asgn.employee_number = emp.employee_number
+  INNER JOIN Department AS dpt ON asgn.department_id = dpt.id;
+
 -- ------------
 
 /* ----
